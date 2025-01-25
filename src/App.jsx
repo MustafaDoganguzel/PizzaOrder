ed18b4612958322491578ab2e6922e62ff6ddc05
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-01-26 01:18:58 +0300

    App.jsx revised
    Switch and Route added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import { useState } from 'react'
 
 
 import HomePage from './components/HomePage/HomePage'
+import OrderPage from './components/OrderPage/orderPage'
+import { Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom/cjs/react-router-dom.min'
 
 function App() {
 
 
   return (
     <>
-      <HomePage />
+      <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Route path='/OrderPage'>
+          <OrderPage />
+        </Route>
+      </Switch>
+
+
     </>
   )
 }
