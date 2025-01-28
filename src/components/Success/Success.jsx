92412790926ea4b98f5f56df1f638b9727b3e345
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-01-28 09:44:30 +0300

    Success css and jsx is created

---
+++ src/components/Success/Success.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Success.css'
+export default function Success() {
+    return (<>
+
+        <header className='success'>
+            <img className='success-logo' src="../images/iteration-1-images/logo.svg" alt="home-logo" />
+        </header>
+
+        <div className='congrats'>
+            <h1>TEBRIKLER</h1>
+            <h1>SIPARISINIZ ALINDI!</h1>
+        </div>
+
+    </>
+    )
+}
