761957400e1be915d0157dff50319b71dc9c5e2b
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-01-28 16:52:31 +0300

    Success css and jsx revision

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Success.css'
-export default function Success() {
+export default function Success(props) {
+    const { responseData } = props;
+    const { thickness,
+        size,
+        additional,
+        adSoyad,
+        note } = responseData
     return (<>
 
         <header className='success'>
@@ -10,6 +16,12 @@ export default function Success() {
         <div className='congrats'>
             <h1>TEBRIKLER</h1>
             <h1>SIPARISINIZ ALINDI!</h1>
+            <div>
+                <p>Boyut: <span>{size}</span></p>
+                <p>Hamur: <span>{thickness}</span></p>
+                <p>Ek Malzemeler: <span>{additional.join(", ")}</span></p>
+            </div>
+
         </div>
 
     </>
